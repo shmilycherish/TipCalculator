fb4d9f2af2c5d0972ddb1b6a8e5239596ed0209d
Cherish <shmilycherish@gmail.com>
2015-04-27 23:16:50 +0800

    add tip calculator model

---
+++ TipCalculator/TipCalculatorModel.swift
@@ -0,0 +1,42 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by cherish on 4/27/15.
+//  Copyright (c) 2015 foreverYoung. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double]{
+        let possibleTipsInfered = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        
+        for possibleTip in possibleTipsInfered {
+            let intPct = Int(possibleTip * 100)
+            
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+}
